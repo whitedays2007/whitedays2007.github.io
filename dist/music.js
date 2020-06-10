53a752bfb332335e287681a047a8f8c5e9b773cf
whitedays2007 <win2008summer@gmail.com>
2020-06-10 18:00:41 +0800

    Site updated: 2020-06-10 18:00:40

---
+++ dist/music.js
@@ -0,0 +1,76 @@
+const ap = new APlayer({
+	    container: document.getElementById('aplayer'),
+	    fixed: true,
+	    autoplay: false,
+	    audio: [
+		    	  {
+				          name: '少年の夜',
+				          artist: 'Bassy',
+				          url: 'http://music.163.com/song/media/outer/url?id=699916.mp3',
+				          cover: 'http://p2.music.126.net/67mTmtrtN1rIdsiAV_YfvA==/923589767332111.jpg',
+				        },
+		          {
+				          name: "Lazy Afternoon",
+				          artist: 'ElyouBeats',
+				          url: 'http://music.163.com/song/media/outer/url?id=29049489.mp3',
+				          cover: 'http://p2.music.126.net/C_I6JDY4WQItRxlAuUyA2g==/3228166139854203.jpg',
+				        },
+		          {
+				          name: 'Neonland',
+				          artist: 'Detektivbyrån',
+				          url: 'http://music.163.com/song/media/outer/url?id=19648867.mp3',
+				          cover: 'http://p2.music.126.net/IHKpTJ1z0SDahH2kChYz8g==/109951163110397875.jpg',
+				        },
+		          {
+				          name: 'Late Night Bus Rlde',
+				          artist: 'ElyouBeats',
+				          url: 'http://music.163.com/song/media/outer/url?id=27707271.mp3',
+				          cover: 'http://p1.music.126.net/HN65W22aJd-6zieATKLiVA==/5754843859868039.jpg',
+				        },
+		    	  {
+				          name: '蝉鸣之夏',
+				          artist: '清风至Verse',
+				          url: 'http://music.163.com/song/media/outer/url?id=864634875.mp3',
+				          cover: 'http://p2.music.126.net/8Fx2oXWF0nYlE5l9QWRRTw==/109951163393561441.jpg',
+				        },
+		    	  {
+				          name: 'Upon My Soul',
+				          artist: 'Organic Stereo',
+				          url: 'http://music.163.com/song/media/outer/url?id=486302.mp3',
+				          cover: 'http://p1.music.126.net/7Z-KzV2Rtfb5RX-lBrIUWQ==/109951164075810813.jpg',
+				        },
+		    	  {
+				          name: 'いのちの名前',
+				          artist: '広橋真紀子',
+				          url: 'http://music.163.com/song/media/outer/url?id=578090.mp3',
+				          cover: 'http://p2.music.126.net/EjmtoVVacC8hpP2Tnpeqiw==/109951163338317773.jpg',
+				        },
+   				  {
+				          name: 'key Rewrite op-Philosophyz',
+				          artist: '同人音乐',
+				          url: '/music/【钢琴】key Rewrite op-Philosophyz 哲♂学.m4a',
+				          cover: 'https://y.gtimg.cn/mediastyle/music_v11/extra/default_300x300.jpg?max_age=31536000',
+				        },
+				   {
+				          name: 'Twinkle Train Take Us Home',
+				          artist: '麗美 (れいみぃ)',
+				          url: 'http://music.163.com/song/media/outer/url?id=4924847',
+				          cover: 'http://p1.music.126.net/U9gT1rPyWBnKKC49SN_eRQ==/5771336534393877.jpg',
+				        },
+				   {
+				   		  name: 'Bad Apple!! (弦楽合奏モノクロ版)',
+				   		  artist: '最新热歌慢摇114',
+				   		  url: 'http://music.163.com/song/media/outer/url?id=409941020',
+				   		  cover: 'http://p2.music.126.net/xXXsTafARePvyQUDb6q9HQ==/3386495814644488.jpg',
+
+				   		},
+				   {
+				   		  name: 'V.A.-樱花祭',
+				   		  artist: '樱花祭',
+				   		  url: 'http://music.163.com/song/media/outer/url?id=1435247742.mp3',
+				   		  cover: 'http://p2.music.126.net/Iq046W9ZRZRLdk0-hRzSoA==/109951164852202041.jpg',
+				   		},
+		        ]
+});
+
+
